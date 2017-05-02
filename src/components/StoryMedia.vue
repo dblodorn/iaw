9d3f48cc12a1e48e7b4b6185c9fc5e35c9270a1a
Dain Blodorn <dain@db13.us>
2017-05-02 16:57:57 -0700

    morer searchings

---
+++ src/components/StoryMedia.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  aside#bg-image(:style="{ 'background-image': 'url(' + media.background + ')' }")
+</template>
+
+<script>
+export default {
+  name: 'story-media',
+  props: ['media'],
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="sass">
+  @import "../_sass/utilities/_utilities.sass"
+  
+  aside
+    height: calc(100vh - #{$header-height})
+    position: fixed
+    top: $header-height
+    left: 0
+    width: 100vw
+    z-index: 0
+    background-size: cover
+    background-position: center
+    background-repeat: no-repeat
+
+</style>
