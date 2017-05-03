138489ed37cb4ce106e1f9644feca807479d1db8
Dain Blodorn <dain@db13.us>
2017-05-03 12:46:40 -0700

    added audio

--- src/components/StoryMedia.vue
+++ src/components/StoryMedia.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-  aside#bg-image(:style="{ 'background-image': 'url(' + media.background + ')' }")
+  #media-component
+    aside#bg-image(:style="{ 'background-image': 'url(' + media.background + ')' }")
+    audio#audio-player(controls autoplay)
+      source(v-bind:src="media.audio")
 </template>
 
 <script>
@@ -15,6 +18,12 @@ export default {
 <style lang="sass" scoped>
   @import "../_sass/utilities/_utilities.sass"
   
+  audio
+    position: fixed
+    bottom: 0
+    width: 100vw
+    z-index: 9000
+
   aside#bg-image
     +bg-image
 
