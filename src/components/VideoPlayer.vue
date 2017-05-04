9e57bfc45b2321bbdb920559a55ad508a7385740
Dain Blodorn <dain@db13.us>
2017-05-04 11:09:00 -0700

    added simple video loop player

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,67 +1,28 @@
 <template lang="pug">
-  #video-bg
-    video#video-player-once(v-if="videosrc.loop == 'false'")
-      source(v-bind:src="videosrc.src")
-    video#video-player-loop(v-if="videosrc.loop == 'true'" loop)
-      source(v-bind:src="videosrc.src")
+  video(v-bind:id="'video-' + index")
+    source(v-bind:src="videosrc.src")
 </template>
 
 <script>
   export default {
     name: 'video-player',
-    props: ['videosrc'],
+    props: ['videosrc','index'],
     data () {
       return {}
     },
     mounted: function () {
-      const videoOnce = document.getElementById('video-player-once')
-      const videoLoop = document.getElementById('video-player-loop')
-      if(videoOnce) {
-        const video = videoOnce
-        const videoPlayOnce = () => {
-          video.play()
-          video.onended = () => {
-            console.log('video over')
-          }
-        }
-        videoPlayOnce()
-      }
-      if(videoLoop) {
-        const video = videoLoop
-        const videoPlayLoop = () => {
-          video.play()
-        }
-        videoPlayLoop()
-      }
+      console.log('video mounted')
     }
   }
 </script>
 
 <style lang="sass" scoped>
   @import "../_sass/utilities/_utilities.sass"
+
+  video
+    opacity: 0
   
-  #video-bg
-    display: block
-    position: fixed
-    top: 0
-    left: 0
-    z-index: 0
-    height: 100vh
-    width: 100vw
-    overflow: hidden
+  .video-visible
     opacity: 1
-    z-index: 0
-    video
-      position: absolute
-      min-width: 100%
-      min-height: 100%
-      width: auto
-      height: auto
-      z-index: -1000
-      overflow: hidden
-      top: 50%
-      left: 50%
-      transform: translate(-50%, -50%)
-      background-color: rgba(0,0,0,0)
 
 </style>
---
+++ src/components/VideoPlayerLoop.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  video#video-loop(loop)
+    source(v-bind:src="videosrc.src")
+</template>
+
+<script>
+  export default {
+    name: 'video-player-loop',
+    props: ['videosrc'],
+    data () {
+      return {}
+    },
+    mounted: function () {
+      console.log('video loop mounted')
+    }
+  }
+</script>
